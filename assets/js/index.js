c2148cd4a6a7d31b01895a542f53c461b8155ba8
The Blob SCP <81673375+theblobscp@users.noreply.github.com>
2021-07-08 18:14:28 -0400

    Removed unneeded js content

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,1 @@
-const json = {
-  projectname: "Server-Page",
-  libraries: ["bootstrap", "scss"],
-};
-const libraries = json.libraries;
-const bootstrap = libraries[0];
-const scss = libraries[1];
-const project = json.projectname;
+//JavaScript coming soon lol
